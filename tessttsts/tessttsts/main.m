0bd367e93dff0ad8662812d080cf2ae8378268e3
Darren Xu <darrentennis@163.com>
2018-02-04 00:09:52 +0800

    测试题叫

---
+++ tessttsts/tessttsts/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tessttsts
+//
+//  Created by Darren  xu on 2018/2/4.
+//  Copyright © 2018年 Darren  xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
